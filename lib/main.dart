2c3bb2c4c1e42719e2852004e51629c2440309ad
24shrouk <shroukhegab3@gmail.com>
2025-02-20 19:57:56 +0200

    add_logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gradprj/core/routing/routes.dart';
+import 'package:gradprj/core/routing/routing.dart';
 
 void main() {
-  runApp(const GraduationProject());
+  runApp(GraduationProject(
+    routing: Routing(),
+  ));
 }
 
 class GraduationProject extends StatelessWidget {
-  const GraduationProject({super.key});
+  final Routing routing;
+  const GraduationProject({super.key, required this.routing});
 
   @override
   Widget build(BuildContext context) {
