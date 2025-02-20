36bad2d74c796029185dacfee7bc6b5ca8b79361
24shrouk <shroukhegab3@gmail.com>
2025-02-20 19:13:59 +0200

    create colors and fonts

---
+++ lib/core/theming/myFonts.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyFontStyle {
+  static TextStyle font12RegularBtn =
+      GoogleFonts.raleway(fontSize: 12.8, fontWeight: FontWeight.w400);
+  static TextStyle font13RegularBtnTxt =
+      GoogleFonts.raleway(fontSize: 13.6, fontWeight: FontWeight.w400);
+  static TextStyle font13RegularAcc =
+      GoogleFonts.raleway(fontSize: 13.4, fontWeight: FontWeight.w400);
+  static TextStyle font13BoldUnderline =
+      GoogleFonts.raleway(fontSize: 13.4, fontWeight: FontWeight.w700);
+  static TextStyle font11RegularUnderline =
+      GoogleFonts.raleway(fontSize: 11.5, fontWeight: FontWeight.w400);
+  static TextStyle font45Regular =
+      GoogleFonts.raleway(fontSize: 45.2, fontWeight: FontWeight.w400);
+  static TextStyle font45Bold =
+      GoogleFonts.raleway(fontSize: 45.2, fontWeight: FontWeight.w700);
+  static TextStyle font38Bold =
+      GoogleFonts.raleway(fontSize: 38.4, fontWeight: FontWeight.w400);
+  static TextStyle font38Regular =
+      GoogleFonts.raleway(fontSize: 38.4, fontWeight: FontWeight.w700);
+  static TextStyle font9RegularAcc =
+      GoogleFonts.raleway(fontSize: 9.3, fontWeight: FontWeight.w400);
+  static TextStyle font9BlodAcc =
+      GoogleFonts.raleway(fontSize: 9.3, fontWeight: FontWeight.w700);
+}
